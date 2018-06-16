0963da4a21c609278f7f7ee4bb80e2b8f44971c1
Soheil Novinfard <novinfard@gmail.com>
2018-06-16 02:47:54 +0100

    Add TextInput and Greeting text

--- App.js
+++ App.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, View, Container, Content, Form, Item, Input, Label, Button, Text, Image, TextInput} from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+            <Image
+            style={styles.logo}
+            source={{uri: 'https://www.ourpath.co.uk/img/logo/logo-blue-side.png'}}
+            />
+            <Text style={styles.head1}>Hello, my name is {this.state.text}</Text>
+            <TextInput
+            style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 300, marginTop: 50}}
+            onChangeText={(text) => this.setState({text})}
+            value={this.state.text}
+            />
+            </View>
+        );
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = { text: '' };
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        padding: 20,
+    },
+    logo: {
+        margin: 100,
+        width: 200,
+        height: 52,
+    },
+    head1: {
+        fontSize: 25,
+    }
 });
